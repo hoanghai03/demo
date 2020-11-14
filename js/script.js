b9fb9b9f248e9d37ba11ee5f7e31c623b9bfe70e
Cuong Nguyen <cuongnv.9899@gmail.com>
2020-11-14 12:26:45 +0700

    1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+/* --------- detecting scroll --------- */
+var header = document.getElementsByTagName('header')[0];
+window.onscroll = function() {
+    if(window.scrollY != 0) {
+        header.classList.add('isFixed');
+    }else {
+        header.classList.remove('isFixed');
+    }
+}
